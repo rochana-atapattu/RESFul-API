0ae454e9472ed1637ff2de3506e992c94a670d47
rochana <rochana.atapattu2@gmail.com>
2018-05-08 18:53:44 +0530

    routes/user was edited

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 //sub package in express to handle routes
-const router = express.Router()
+const router = express.Router();
+const productController = require('../controllers/user');
 
 
 
-router.post('/signup', );
-router.post('/login', );
+router.post('/signup',productController.signup() );
+router.post('/login', productController.login());
 
 
 //router with routes is exported
